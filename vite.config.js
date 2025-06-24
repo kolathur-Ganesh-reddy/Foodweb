98927ecef3da99899457e00cd292cafd595e56f2
Ganesh Reddy <kganireddy28@gmail.com>
2025-06-24 12:14:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Foodweb/',  // Use your GitHub repo name here
+  base: '/Foodweb/',
   plugins: [react()],
 })
